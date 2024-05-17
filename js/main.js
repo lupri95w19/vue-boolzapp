2860719d7163372432adc7cdda5a2b649e8baa4d
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-17 11:50:27 +0200

    Milestone 1 - Tante cose fatte, e tanti esaurimenti

--- js/main.js
+++ js/main.js
@@ -15,45 +15,93 @@ const configurazione = {
         return {
             myProfile: 'Luca',
             immagine: './img/Io.png',
-            ourMsg : ['Ehy', 'Come va? tutto bene?', 'Elemento 3'],
+            ourMsg: ['Ehy', 'Come va? tutto bene?', 'Elemento 3'],
             nomeContatto: 'prova',
             contatto: [
                 {
+                    // nome: 'Luca',
+                    // img: './img/Io.png',
+                    // ourMsgs: [
+                    //     {
+                    //         // prova: ['Ehy', 'Come va? tutto bene?', 'Elemento 3']
+                    //         ciao : "prova",
+                    //     }
+                    // ]
+
+                    nome: 'Alberto',
+                    msg: [
+                        {
+                            data : '10/01/2020 15:30:55',
+                            messaggio: 'Hai portato a spasso il cane?',
+                            stato: 'sent'
+            
+                        },
+                        {
+                            data : '10/01/2020 15:40:55',
+                            messaggio: 'Rispondimi?',
+                            stato: 'sent'
+                        },
+                        {
+                            data : '10/01/2020 15:30:55',
+                            messaggio: 'Va bene a dopo?',
+                            stato: 'received'
+                        }                        
+                    ],
+                    img: './img/avatar_1.jpg',
+                    ultimoAccesso: 'oggi alle 12:00',
+                    value: "user",
+                    ownUser: false
+                },
+                {
                     nome: 'Gigi',
-                    msg : ['Ehila'],
-                    img : './img/avatar_1.jpg',
-                    ultimoAccesso : 'oggi alle 12:00'
+                    msg: ['Ehila'],
+                    img: './img/avatar_1.jpg',
+                    ultimoAccesso: 'oggi alle 12:00',
+
                 },
                 {
                     nome: 'Fabio',
-                    msg : ['Sto arrivando'],
-                    img : './img/avatar_2.jpg',
-                    ultimoAccesso : 'ultimo accesso ieri alle 13:00'
+                    msg: ['Sto arrivando'],
+                    img: './img/avatar_2.jpg',
+                    ultimoAccesso: 'ultimo accesso ieri alle 13:00',
+
                 },
                 {
                     nome: 'Manuel',
-                    msg : ['No oggi non ho voglia'],
-                    img : './img/avatar_3.jpg',
-                    ultimoAccesso : 'ultimo accesso martedì alle 13:00'
+                    msg: ['No oggi non ho voglia'],
+                    img: './img/avatar_3.jpg',
+                    ultimoAccesso: 'ultimo accesso martedì alle 13:00',
+
                 },
                 {
                     nome: 'Alessandro B.',
-                    msg : ['Molto interessante'],
-                    img : './img/avatar_4.jpg',
-                    ultimoAccesso : 'online'
+                    msg: ['Molto interessante'],
+                    img: './img/avatar_4.jpg',
+                    ultimoAccesso: 'online',
+
                 },
                 {
                     nome: 'Fabio',
-                    msg : ['Sto arrivando'],
-                    img : './img/avatar_2.jpg',
-                    ultimoAccesso : 'ultimo accesso ieri alle 13:00'
+                    msg: ['Sto arrivando'],
+                    img: './img/avatar_2.jpg',
+                    ultimoAccesso: 'ultimo accesso ieri alle 13:00',
+
                 },
             ]
         }
     },
+
+
+    methods: {
+        checkImageVisibility(index) {
+            console.log("ciaooo" + this.contatto[index].ownUser);
+            // Solamente il proprio utente ha .ownUser, quindi solamente quello avrà active, che su css è display-none
+            return (this.contatto[index].ownUser == false) ? 'active' : 'hngnhgnghngh';
+        }
+    }
 };
 
-console.log (this.ourMsg)
+console.log(this.ourMsg)
 
 
 
